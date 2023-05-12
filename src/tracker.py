e608a1d087c4110f996a08a7950caf74292f745b
Skibin Maksim <ski6a_23@mail.ru>
2023-05-12 23:33:39 +0300

    points tracker: add append

--- src/tracker.py
+++ src/tracker.py
@@ -3,3 +3,6 @@ class PositionTracker:
     def __init__(self, n_to_hold: int):
         self.n_to_hold = n_to_hold
         self.cords = []
+
+    def append(self, xy: tuple):
+        self.cords = (self.cords + [xy])[-self.n_to_hold:]
